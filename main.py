90f9c82c3ffaf48954780b756f567b35b02fb3ba
agx <recklight>
2022-03-15 10:16:05 +0800

    LTP-v1.0.3-2020.03.14-agx44

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-# -*- coding: utf-8 -*-
+# This is a sample Python script.
 
-"""
-@ author: recklight
-@ contact:recognizer.light@gmail.com
-@ version: n
-@ license: n
-@ file: main.py
-"""
-
-import sys
-import argparse
-from ssRobotApp import ssRobotApp, QApplication
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def parse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--reset', action='store_true', help="Reset GUI settings.")
-    args = parser.parse_args()
-    return args
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # args = parse()
-    app = QApplication(sys.argv)
-    window = ssRobotApp()
-    # window.read_settings(reset=args.reset)
-    window.show()
-    app.exec_()
-    # window.write_settings()
-    sys.exit()
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
